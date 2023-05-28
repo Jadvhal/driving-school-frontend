cdb9227c461728608c50676fcb60302cc35c0ba9
Anass BOURASSE <50037710+Jadvhal@users.noreply.github.com>
2023-05-28 13:57:13 +0100

    Update PrimarySection.tsx

--- src/components/pages/Home/PrimarySection/PrimarySection.tsx
+++ src/components/pages/Home/PrimarySection/PrimarySection.tsx
@@ -33,7 +33,10 @@ const PrimarySection: FC = () => {
   <p className="text-center">
     Add some text
   </p>
-  <textarea className="w-full p-10 border border-blue-900 rounded resize-none text-sm bg-blur shadow-lg" style={{ height: "300px" }}></textarea>
+<textarea
+  className="w-full p-10 border border-blue-900 rounded resize-none text-sm bg-blur-gradient shadow-lg"
+  style={{ height: "300px" }}
+></textarea>
   <div className="flex justify-between mt-4">
     <Link href="/#form" scroll={false}>
       <Button primary>
